88cf09aa73689533e1ed1becab103a234658f66a
Adam Bridge <bridge.adam@gmail.com>
2024-04-15 11:39:35 -0400

    Add mod pow 2 exercise

--- 01-primitive-types/1.1/variants.py
+++ 01-primitive-types/1.1/variants.py
@@ -1,3 +1,7 @@
+"""
+Using only bitwise operators, equality checks and boolean operators
+"""
+
 def right_propagate(x):
     """
     One stragegy:
@@ -9,9 +13,25 @@ def right_propagate(x):
     or/xor with original
     01011111
     >>> print(f"{right_propagate(0b01010000):b}")
-    01011111
+    1011111
     """
     rightmost_bit = x & ~(x - 1)
     return x | rightmost_bit - 1
     
+
+def mod_pow_2(x, n):
+    """
+    Compute x mod 2 ** n
+    x: 77, n: 6 => 77 mod 2 ** 6 => 77 mod 64 => 13
+    Strategy: 
+    1. 2 ** n => 1 << n
+    2. modulus
+    77 => 1001101
+    64 => 1000000
+    modulus is 77 xor 64
+       => 0001101
+    >>> mod_pow_2(77, 6)
+    13
+    """
+    return x ^ 1 << n
 
