44879b37e59fd8e1ac122a2cd656a0d9b1dd1fae
Adam Bridge <bridge.adam@gmail.com>
2024-05-07 13:48:40 -0400

    Cool one-liner solution from book

--- 12-hash-tables/palindromic_permutations.py
+++ 12-hash-tables/palindromic_permutations.py
@@ -23,6 +23,14 @@ def has_palindromic_permutations(s):
     return result
 
 
-
-
+def can_form_palindrome(s):
+    """
+    Neat one line solution is possible using generator expression (similar syntax to list
+    comprehension) and adding up frequencies mod 2 (0 for all even).
+    >>> has_palindromic_permutations("enough enough")
+    True
+    >>> has_palindromic_permutations("want to go on")
+    False
+    """
+    return sum(freq % 2 for freq in Counter(s).values()] <= 1
 
