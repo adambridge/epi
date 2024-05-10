fb989ce58a45032ead8a340dccf8c5a2f3130710
Adam Bridge <bridge.adam@gmail.com>
2024-05-10 10:03:21 -0400

    Ex 12.2 - neat pythonic solution

--- 12-hash-tables/anonymous_letter.py
+++ 12-hash-tables/anonymous_letter.py
@@ -1,12 +1,23 @@
 from collections import Counter
 
 def is_constructible(letter, magazine):
+    letter_counter, magazine_counter = Counter(letter), Counter(magazine)
     """
     Return True if ransom note `letter` can be contructed from letters in `magazine` else False
+    (My attempt)
     >>> is_constructible("money by noon", "cats are good")
     False
     >>> is_constructible("money by noon", "no one needs to buy yams")
     True
     """
-    letter_counter, magazine_counter = Counter(letter), Counter(magazine)
     return all(lv <= magazine_counter[lk] for lk, lv in letter_counter.items()) 
+
+def book_contstructible(letter, magazine):
+    """
+    Neat pythonic book solution
+    >>> is_constructible("money by noon", "cats are good")
+    False
+    >>> is_constructible("money by noon", "no one needs to buy yams")
+    True
+    """
+    return not Counter(magazine) - Counter(letter)
