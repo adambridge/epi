1689a5b3ebc2cc09fe7b9c560bb5e9e4b363d346
Adam Bridge <bridge.adam@gmail.com>
2024-05-13 11:50:15 -0400

    Book brute force solution to 13.1

--- 13-sorting/intersect.py
+++ 13-sorting/intersect.py
@@ -2,10 +2,10 @@ def my_intersect(a, b):
     """
     Return the intersection of two sorted lists (no duplicates) my attempt
     Iterate both lists, if elem equal append to new, increment if either equal
-    to last added, if not equal increment lower (brutish solution?)
-    Time complexity O(len(a) * len(b))
-    >>> my_intersect([0,1,2,3,4,5], [2,2,3])
-    [2, 3]
+    to last added, if not equal increment lower (brutish solution? nope)
+    Correction: time complexity not O(len(a) * len(b)) but O(len(a) + len(b))
+    >>> my_intersect([0,1,2,2,3,3,4,5,6], [2,2,3,4,4,5])
+    [2, 3, 4, 5]
     """
     i, j, result, last_added = 0, 0, [], None
     while True:
@@ -28,3 +28,12 @@ def my_intersect(a, b):
 
     return result
 
+
+def book_intersect(A, B):
+    """
+    So smol but inefficient O(len(a) * len(b)) time since searches whole of B for each in A
+    enumerate used to look at last elem and remove duplicates
+    >>> my_intersect([0,1,2,2,3,3,4,5,6], [2,2,3,4,4,5])
+    [2, 3, 4, 5]
+    """
+    return [a for i, a in enumerate(A) if a in B and (i == 0 or A[i] != A[i - 1])]
