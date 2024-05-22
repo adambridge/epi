5805b7e8c7e038d376ecfe2fd7a3ec86ebaef4e8
Adam Bridge <bridge.adam@gmail.com>
2024-05-22 11:39:58 -0400

    Towers of Hanoi solution

--- 15-recursion/hanoi.py
+++ 15-recursion/hanoi.py
@@ -1,7 +1,4 @@
 
-# initial configuration:
-towers = ((6,5,4,3,2,1),(),())
-
 """
 Value of element is width of disc
 Last element is top of tower
@@ -9,6 +6,12 @@ Refer to towers by index: 0, 1, 2
 Print sequence to move discs from 0 to 1
 
 examples:
+
+  0    1  2
+((2,1),(),())   target: 1
+((2),(),(1))
+((),(2),(1))
+((),(2,1),())
 
 ((3,2,1),(),())
 ((3,2),(1),())
@@ -18,5 +21,32 @@ examples:
 ((1),(3),(2))
 ((1),(3,2),())
 ((),(3,2,1),())
+
+General pattern for n rings:
+1. move n - 1 rings to intermediary (i.e. peg that is not target) using target as intermediary
+2. move nth ring to target
+3. move n - 1 rings to target from intermediary
+
+-> 1 and 3 can each be individually permormed using this pattern -> recursion
+"""
+
+
+def list_moves(towers, n, fro, to, via):
+    """
+    My reimplementation of book solution 
+    >>> list_moves([[2,1],[],[]], 2, 0, 1, 2)
+    [[0, 2], [0, 1], [2, 1]]
+    """
+
+    def move_rings(n, fro, to, via):
+        if n > 0:
+            move_rings(n - 1, fro, via, to)
+            towers[to].append(towers[fro].pop())
+            moves.append([fro, to])
+            move_rings(n - 1, via, to, fro)
+
+    moves = []
+    move_rings(n, fro, to, via)
+    return moves
 
 
